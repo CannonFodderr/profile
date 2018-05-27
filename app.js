4c35e32acc4e0ceace460c1de835a0aee1e33bb3
CannonFodderr <izhaki.idan@gmail.com>
2018-05-27 14:04:12 +0300

    Init packages and basic server + db setup

---
+++ app.js
@@ -0,0 +1,32 @@
+const   express                 = require('express'),
+        app                     = express(),
+        router                  = express.Router(),
+        path                    = require('path'),
+        dotenv                  = require('dotenv').config(),
+        bodyParser              = require('body-parser'),
+        methodOverride          = require('method-override'),
+        mongoose                = require('mongoose'),
+        passport                = require('passport'),
+        LocalStrategy           = require('passport-local'),
+        passportLocalMongoose   = require('passport-local-mongoose'),
+        port                    = process.env.PORT,
+        dbURL                   = process.env.DBURL;
+
+mongoose.connect(dbURL)
+.then((connection)=>{console.log('Connected to DB')})
+.catch((err)=>{
+    console.error(err);
+});
+
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname + '/public')));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(methodOverride("_method"));
+
+app.get('/', (req, res)=>{
+    console.log('home');
+    res.send('This will be the homepage');
+});
+
+app.listen(port, (req, res)=> console.log(`Server is running on ${port}`));
+
