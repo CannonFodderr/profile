d1044e4a273a9dc6bfda17ac9c6b04b8dd7fcde0
CannonFodderr <izhaki.idan@gmail.com>
2018-05-28 17:51:01 +0300

    Add basic index css

---
+++ public/scripts/main.js
@@ -0,0 +1,43 @@
+const body = document.getElementsByTagName('body');
+const anchors = document.getElementsByClassName('linkbtn');
+let opIn = 0.1;
+let opOut = 1;
+// =====================
+// Fade In/Out functions
+// =====================
+fadeIn = (element) => {
+        if(opIn <= 1){
+            opIn += 0.01;
+            setTimeout(()=>{
+                element.style.opacity = opIn;
+                fadeIn(element)
+            }, 5)    
+        } else {    
+            return opIn = 0
+        };
+}
+
+fadeOut = (element) => {
+    if(opOut >= 0){
+        opOut -= 0.01;
+        setTimeout(()=>{
+            element.style.opacity = opOut;
+            fadeOut(element)
+        }, 5)    
+    } else {    
+        return opOut = 1
+    };
+}
+
+
+fadeIn(body[0]);
+
+// Loop through all anchors
+for (let anchor of anchors){
+    anchor.addEventListener('click', ()=>{
+        fadeOut(body[0]);
+        setTimeout(()=>{
+            window.location.href = anchor.id;
+        }, 500)
+    });
+}
